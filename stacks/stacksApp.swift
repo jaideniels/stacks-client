1e0ba322bc1de05c5a3f4a6cc668c97f40ccde25
Jay Daniels <jaymdan@gmail.com>
2021-12-09 14:24:29 -0800

    Initial Commit

---
+++ stacks/stacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  stacksApp.swift
+//  stacks
+//
+//  Created by jaydan on 12/9/21.
+//
+
+import SwiftUI
+
+@main
+struct stacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
